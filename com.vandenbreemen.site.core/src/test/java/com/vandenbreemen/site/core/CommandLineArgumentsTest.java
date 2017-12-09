82cb774b27eaae055f371c2bf690e9cdeca9e5c1
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2017-12-09 12:16:00 -0500

    Additional verification

--- com.vandenbreemen.site.core/src/test/java/com/vandenbreemen/site/core/CommandLineArgumentsTest.java
+++ com.vandenbreemen.site.core/src/test/java/com/vandenbreemen/site/core/CommandLineArgumentsTest.java
@@ -22,4 +22,12 @@ public class CommandLineArgumentsTest {
         assertEquals("Argument value expected", "Command", args.valueOf("c").get());
     }
 
+    @Test
+    public void testParseMultiwordArgument(){
+        CommandLineArguments args = new CommandLineArguments(new String[]{
+                "-c", "This is a test"
+        });
+        assertEquals("Argument value expected", "This is a test", args.valueOf("c").get());
+    }
+
 }
